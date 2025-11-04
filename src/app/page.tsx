d4a4da34056a56ddb3235f68a1d9b19efe6c82e1
LucasChambi <lucasbrunocc880@outlook.com>
2025-11-03 22:01:25 -0300

    Criando portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div className="min-h-screen font-bold text-white p-10">
+    <div className="min-h-screen font-bold text-white p-10 text-center">
       <h1 className="text-center font-sans">Olá, meu nome é Lucas Bruno Calle Chambi</h1>
       <p className="text-center">Desenvolvedor Web</p>
 
