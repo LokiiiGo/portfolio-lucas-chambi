8949affe8b77df2c4489dcf229f5ecf834e6cd03
LucasChambi <lucasbrunocc880@outlook.com>
2025-11-13 18:24:21 -0300

    Criando portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,46 @@
+import Link from 'next/link';
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="p-5 text-white min-h-screen bg-zinc-800 bg-[url(/space-universo.jpg)] bg-cover">
-      <header className="m-10 text-center mb-10">
-        <a href="#home" className="m-10">Home</a>
-        <a href="#about" className="m-10" >About</a>
-        <a href="#projetos" className="m-10">Projetos</a>
-        <a href="#contato" className="m-10">Contato</a>
-        <div className="mb-15"></div>
-        <h1 className="text-center font-sans text-4xl mb-5">Olá, sou Lucas Bruno Calle Chambi</h1>
-        <p className="text-center text-3xl">Desenvolvedor Front-End</p>
+    <div>
+      <header className="text-center text-white mt-10">
+        <Link href="#" className="mr-10">Home</Link>
+        <Link href="#about" className="mr-10">Sobre</Link>
+        <Link href="#projetos" className="mr-10">Projetos</Link>
+        <Link href="#contato" className="mr-10 ">Contato</Link>
       </header>
 
-      <main className="bg-stone-200 border-b-gray-950 p-10 rounded-lg text-black">
-        <section id="#about" className="mb-20">
-          <h2 className="text-2xl mb-5 text-center">Sobre Mim</h2>
-          <p className="text-center">Sou um desenvolvedor front-end com experiência em criar interfaces de usuário atraentes e funcionais. Tenho paixão por tecnologia e estou sempre buscando aprender novas habilidades.</p>
-          <div className="mb-20"></div>
-          <h2 className="text-2xl text-center">Minhas Habilidades</h2>
-          <div className="flex flex-row text-center mt-2 flex-wrap justify-center gap-2">
-            <div className="basis-64">PHP</div>
-            <div className="basis-64">BLADE</div>
-            <div className="basis-64">TAILWINDCSS</div>
-            <div className="basis-64">JAVASCRIPT</div>
-            <div className="basis-64">REACTJS</div>
-            <div className="basis-64">NEXTJS</div> 
-          </div>
-        </section>
+      <main className="text-white text-center mt-10">
+        <p className="text-3xl mb-5">Olá, sou Lucas Bruno Calle Chambi</p>
+        <p className="text-3xl mb-5">Desenvolvedor Web</p>
       </main>
+
+      <article className="bg-white p-20 pr-20 pl-20 pt-5 mr-20 ml-20 text-center rounded-lg min-h-screen">
+        <h2 className="text-4xl font-bold text-black pt-1">Sobre mim</h2>
+        <div className="mb-10 border-2 border-black"></div>
+        <p className="text-xl text-black">Sou um desenvolvedor web com foco em front-end e tenho experiência em criar interfaces de usuário atraentes e funcionais.
+          Tenho paixão por tecnologia e estou sempre buscando aprender novas habilidades.</p>
+        <div className="mb-20 pb-15"></div>
+        <h2 className="text-4xl font-bold text-black pt-1">Minhas Habilidades</h2>
+        <div className="mb-10 border-2 border-black"></div>
+        <div className="flex flex-row justify-center">
+          <div className="basis-64 bg-[#474A8A] p-5 rounded-lg mr-5">PHP</div>
+          <div className="basis-64 bg-[#FFE100] p-5 rounded-lg mr-5">JavaScript</div>
+          <div className="basis-64 bg-[#F1892D] p-5 rounded-lg mr-5">HTML</div>
+          <div className="basis-64 bg-[#00C4C1] p-5 rounded-lg mr-5">REACT</div>
+          <div className="basis-64 bg-[#5FBCFF] p-5 rounded-lg mr-5">TAILWINDCSS</div>
+          <div className="basis-64 bg-[#5400C1] p-5 rounded-lg mr-5">CSS</div>
+          <div className="basis-64 bg-[#000000] p-5 rounded-lg mr-5">Next.JS</div>          
+          <div className="basis-64 bg-[#202020] p-5 rounded-lg mr-5">GitHub</div>
+          <div className="basis-64 bg-[#C54F00] p-5 rounded-lg mr-5">Git</div>
+          <div className="basis-64 bg-[#006ECE] p-5 rounded-lg mr-5">MySQL</div>
+        </div>
+      </article>
+      <div className="mb-30"></div>
+      <article className="bg-white p-20 pr-20 pl-20 pt-5 mr-20 ml-20 text-center rounded-lg min-h-screen">
+        <h2 className="text-4xl font-bold text-black pt-1">Meus Projetos</h2>
+        <div className="mb-10 border-2 border-black"></div>
+      </article>
     </div>
-  );
+  )
 }
