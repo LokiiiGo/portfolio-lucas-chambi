bcac034a3e7d846d64da906b4e623a16ef16809a
LokiiiGo <lucascalle880@gmail.com>
2026-01-17 21:39:07 -0300

    Alterando ícone e background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="icon" href="./portfolio.png" />
+      <link rel="icon" href="./portifolio.png" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
